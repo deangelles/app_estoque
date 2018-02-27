3021a952b811d83b3f4b6a293154f0081c0ec282
Deangelles Debastiani Cordeiro <deangelles123456@gmail.com>
2018-02-26 20:42:05 -0400

    alterações do dia 26/02/2018

--- App/DAO/ProdutoDAO.php
+++ App/DAO/ProdutoDAO.php
@@ -9,7 +9,21 @@
 namespace App\DAO;
 
 
-class ProdutoDAO
+class ProdutoDAO extends Conexao
 {
+    public function inserir($produto)
+    {
+        $sql = "insert into protudos (descricao, quantidade, valor, validade) values (:descricao, :quantidade,:valor,:validade)";
+        try{
+            $i = $this->conexao->prepare($sql);
+            $i->bindValue(":descricao", $produto->getDescricao ());
+            $i->bindValue(":quantidade", $produto->getquantidade ());
+            $i->bindValue(":valor", $produto->getvalor ());
+            $i->bindValue(":validade", $produto->getvalidade ());
+
+        } catch (\PDOException $e){
+            echo "<div class='alert-danger'>{$e->getMessage()}</div>";
+    }
+    }
 
 }
